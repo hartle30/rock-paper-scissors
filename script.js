89c2c5585241038d3b85722149450b3c82c5a54c
Adam Hartley <adamdhartley@icloud.com>
2022-09-23 11:30:58 -0400

    Create playRound function

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ let playerSelection = prompt('Rock, Paper, Scissors?')
 playerSelection = playerSelection.toLowerCase()
 
 console.log(playerSelection)
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return ('Tie! Choose again.');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return ('You lose! Paper beats rock.');
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return ('You win! Rock beats scissors');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return ('You win! Paper beats rock.');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return ('You lose! Scissors beat rock.') ;
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return ('You lose! Rock beats scissors');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return ('You win! Scissors beat paper.');
+    } else {
+        return ('Please make a valid selection.')
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection))
+

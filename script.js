d48c72ef589ec0cc7cddbb5a98cbe7ff918e343b
Adam Hartley <adamdhartley@icloud.com>
2022-09-23 10:57:43 -0400

    Create playerSelection function

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-// generate computer choice
-// get user choice
 // compare choices
 // determine win, loss, or tie
 // repeat to determine winner of best of 5 series
 
-let computerSelection = Math.floor(Math.random()*3) +1;
-
-function getComputerChoice(computerSelection) {
+// generate computer choice
+function getComputerChoice() {
+    let computerSelection = Math.floor(Math.random()*3) +1;
     if (computerSelection === 1) {
-        return ('Rock');
+        return ('rock');
     } else if (computerSelection === 2) {
-        return ('Paper');
+        return ('paper');
     } else {
-        return ('Scissors')
+        return ('scissors');
     }
 }
 
+computerSelection = getComputerChoice()
+console.log(computerSelection)
+
+//get user choice
 let playerSelection = prompt('Rock, Paper, Scissors?')
+playerSelection = playerSelection.toLowerCase()
+
+console.log(playerSelection)

4ac6e9b76c860d2bc357f859fada0199a72b5aa1
Adam Hartley <adamdhartley@icloud.com>
2022-10-05 11:34:52 -0400

    Update UI with styles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const cpuSelectionDisplay = document.getElementById('cpuSelection');
-const userSelectionDisplay = document.getElementById('userSelection');
+const cpuSelectionDisplay = document.querySelector('.cpuSelection');
+const userSelectionDisplay = document.querySelector('.userSelection');
 const userScore = document.getElementById('userScore');
 const cpuScore = document.getElementById('cpuScore');
 const result = document.getElementById('result');
@@ -12,7 +12,13 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerSelection = button.id;
-        userSelectionDisplay.innerHTML = playerSelection.toUpperCase();
+        if (playerSelection === 'rock') {
+            userSelectionDisplay.innerHTML = '&#9994';
+        } else if (playerSelection === 'paper') {
+            userSelectionDisplay.innerHTML = '&#9995';
+        } else {
+            userSelectionDisplay.innerHTML = '&#9996';
+        }
         resetGame();
         playRound();
     });
@@ -34,7 +40,18 @@ function resetGame() {
 
 function playRound() {
     let computerSelection = getComputerChoice();
-    cpuSelectionDisplay.innerHTML = computerSelection.toUpperCase();
+    let playerState = document.querySelector('.userSelection')
+    let cpuState = document.querySelector('.cpuSelection')
+    playerState.classList.remove('win')
+    cpuState.classList.remove('win')
+ 
+    if(computerSelection === 'rock') {
+        cpuSelectionDisplay.innerHTML = '&#9994';
+    } else if (computerSelection === 'paper') {
+        cpuSelectionDisplay.innerHTML = '&#9995';
+    } else {
+        cpuSelectionDisplay.innerHTML = '&#9996';
+    }
     if (playerSelection === computerSelection) { 
         result.innerHTML = ('Tie! You both chose ' + playerSelection + '.' + ' Choose again.')
     } else if (
@@ -43,18 +60,20 @@ function playRound() {
         (playerSelection === 'scissors' && computerSelection === 'paper')) {
             playerScore +=1;
             userScore.innerHTML = playerScore;
+            playerState.classList.toggle('win');
             if (playerScore === 5) {
                 result.innerHTML = ('Congratulations! You have beat the computer 5 to ' + computerScore + '.')
             } else {
-                result.innerHTML = ('You win! ' + playerSelection + ' beats ' + computerSelection + '.')
+                result.innerHTML = ('You win, ' + playerSelection + ' beats ' + computerSelection + '.')
             }
     } else {
         computerScore +=1;
         cpuScore.innerHTML = computerScore;
+        cpuState.classList.toggle('win')
         if (computerScore === 5) {
             result.innerHTML = ('Bummer! You have lost to the computer 5 to ' + playerScore + '.')
         } else {
-             result.innerHTML = ('You lose! ' + computerSelection + ' beats ' + playerSelection + '.')
+             result.innerHTML = ('You lose, ' + computerSelection + ' beats ' + playerSelection + '.')
         }
     }
 }
